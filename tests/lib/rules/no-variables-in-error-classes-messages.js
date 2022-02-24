cde8ba748bcde412f36070ffcf0a1a9243bf678b
Dominik Gajek <d.gajek@tidio.net>
2022-02-24 14:35:04 +0100

    [BGI-288] - add test case with context variables

--- tests/lib/rules/no-variables-in-error-classes-messages.js
+++ tests/lib/rules/no-variables-in-error-classes-messages.js
@@ -23,7 +23,7 @@ RuleTester.setDefaultConfig({
 });
 var ruleTester = new RuleTester();
 ruleTester.run('no-variables-in-error-classes-messages', rule, {
-    valid: [`new TestError("BYCZQ");`],
+    valid: [`new TestError("BYCZQ");`, `new TestError("BYCZQ", {"projectId": hello});`],
     invalid: [
         {
             code:  `new TestError("BYCZQ" + hello);`,
